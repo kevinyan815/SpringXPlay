481bd2a090ca58aa9425cdba003ac0319e91107e
YanHengjia <hengjia_yan@qingsongchou.com>
2021-06-11 19:26:41 +0800

    update

--- mongo-demo/src/main/java/kev/spring/data/mongodemo/MongoDemoApp.java
+++ mongo-demo/src/main/java/kev/spring/data/mongodemo/MongoDemoApp.java
@@ -3,6 +3,7 @@ package kev.spring.data.mongodemo;
 import com.mongodb.client.result.UpdateResult;
 import kev.spring.data.mongodemo.converter.MoneyReadConverter;
 import kev.spring.data.mongodemo.model.Coffee;
+import kev.spring.data.mongodemo.repository.CoffeeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
@@ -12,6 +13,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -31,6 +33,9 @@ public class MongoDemoApp implements ApplicationRunner {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    @Autowired
+    private CoffeeRepository coffeeRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(MongoDemoApp.class, args);
     }
@@ -42,6 +47,18 @@ public class MongoDemoApp implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        // 使用类似JDBCTemplate的方式进行查询
+        queryMongoUseTemplate();
+        // 使用Repository查询
+        queryMongoUseRepository();
+    }
+
+    /**
+     * 使用类似JDBCTemplate的MongoTemplate进行查询
+     *
+     * @throws InterruptedException
+     */
+    private void queryMongoUseTemplate() throws InterruptedException {
         Coffee espresso = Coffee.builder()
                 .name("espresso")
                 .price(Money.of(CurrencyUnit.of("CNY"), 20.0))
@@ -66,4 +83,34 @@ public class MongoDemoApp implements ApplicationRunner {
 
         mongoTemplate.remove(updateOne);
     }
+
+    /**
+     * 使用Repository模式进行查询
+     */
+    private void queryMongoUseRepository() throws InterruptedException {
+        Coffee espresso = Coffee.builder()
+                .name("espresso")
+                .price(Money.of(CurrencyUnit.of("CNY"), 20.0))
+                .createTime(new Date())
+                .updateTime(new Date()).build();
+        Coffee latte = Coffee.builder()
+                .name("latte")
+                .price(Money.of(CurrencyUnit.of("CNY"), 30.0))
+                .createTime(new Date())
+                .updateTime(new Date()).build();
+
+        coffeeRepository.insert(Arrays.asList(espresso, latte));
+        coffeeRepository.findAll(Sort.by("name"))
+                .forEach(c -> log.info("Saved Coffee {}", c));
+
+        Thread.sleep(1000);
+        latte.setPrice(Money.of(CurrencyUnit.of("CNY"), 35.0));
+        latte.setUpdateTime(new Date());
+        coffeeRepository.save(latte);
+        coffeeRepository.findByName("latte")
+                .forEach(c -> log.info("Coffee {}", c));
+
+        coffeeRepository.deleteAll();
+    }
+
 }
