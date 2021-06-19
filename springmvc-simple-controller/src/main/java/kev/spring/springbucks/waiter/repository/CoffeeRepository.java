45e241acfe1fa6dafd74b5f275bce710759aa64a
kevinyan <hengjia_yan@qingsongchou.com>
2021-06-19 23:23:39 +0800

    add spring mvc demo

---
+++ springmvc-simple-controller/src/main/java/kev/spring/springbucks/waiter/repository/CoffeeRepository.java
@@ -0,0 +1,10 @@
+package kev.spring.springbucks.waiter.repository;
+
+import kev.spring.springbucks.waiter.model.Coffee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
+    List<Coffee> findByNameInOrderById(List<String> list);
+}
