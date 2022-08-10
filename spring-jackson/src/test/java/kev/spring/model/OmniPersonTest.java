15c4c44ca5051678881086a5b4fe03c039c1d5f2
kevinyan <yan@example.com>
2022-08-10 11:57:16 +0800

    update

--- spring-jackson/src/test/java/kev/spring/model/OmniPersonTest.java
+++ spring-jackson/src/test/java/kev/spring/model/OmniPersonTest.java
@@ -5,9 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class OmniPersonTest {
 
